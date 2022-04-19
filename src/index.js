943466c2bd14b97ea93d8745c11230b91ab06794
elizabethvera-atieno <atienolizvera@gmail.com>
2022-04-19 14:41:10 +0300

    complete mini-project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  let listTasks = document.getElementById('tasks')
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    const task = document.getElementById('new-task-description').value
+    let items = document.createElement('li')
+    let btn = document.createElement('button')
+    btn.innerText = 'X'
+    items.innerText = task
+    items.appendChild(btn)
+    listTasks.append(items)
+    btn.addEventListener('click',(e)=>{
+      items.remove()
+    })
+    form.reset()
+  })
 });
